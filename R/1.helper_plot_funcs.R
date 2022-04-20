787ea3dcc34d5d97bc2115a4e1f04deed74b13e1
Yang Liu <lyhello@gmail.com>
2022-04-20 07:25:57 -0400

    update new_cnames to 202 isos

--- R/1.helper_plot_funcs.R
+++ R/1.helper_plot_funcs.R
@@ -190,12 +190,15 @@ get.new_cnames <- function(dir_IGME_input, filename = "country.info.CME.csv"){
 }
 
 
-#' Internal function to update new_cnames.rda (official names)
+#' Internal function to update new_cnames.rda (for official names on the plot)
 #' @importFrom usethis use_data
+#' @param IGME_round_input which IGME round of country.info.CME.csv to use
 #' @param country.info.file.name country.info.CME.csv
 #'
-update.data.new_cnames <- function(country.info.file.name = "country.info.CME.197isos.csv"){
-  new_cnames <- get.new_cnames(get.IGMEinput.dir(2021), country.info.file.name)
+update.data.new_cnames <- function(
+  IGME_round_input = 2022,
+  country.info.file.name = "country.info.CME.csv"){
+  new_cnames <- get.new_cnames(get.IGMEinput.dir(IGME_round_input), country.info.file.name)
   usethis::use_data(new_cnames, overwrite = TRUE)
   return(invisible())
 }
